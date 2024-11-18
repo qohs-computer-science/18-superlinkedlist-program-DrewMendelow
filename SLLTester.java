0ba807d37c46fbd11683b97f605f7d4b6763460e
DrewMendelow <103079010+DrewMendelow@users.noreply.github.com>
2024-11-18 18:39:52 +0000

    work for 11/18

--- SLLTester.java
+++ SLLTester.java
@@ -1,5 +1,30 @@
 class SLLTester {
   public static void main(String[] args) {
+    SuperLinkedList list1 = new SuperLinkedList();
+    SuperLinkedList list2 = new SuperLinkedList();
+    SuperLinkedList list3 = new SuperLinkedList();
+    list1.add("a");
+    list1.add("B");
+    list1.add("c");
+    list1.add("D");
+    list1.add("e");
+    System.out.println("list1:");
+    System.out.println(list1.toString());
 
+    list2.add("D");
+    list2.add("r");
+    list2.add("e");
+    list2.add("w");
+    list2.add("M");
+    System.out.println("list2:");
+    System.out.println(list2.toString());
+
+    list3.add("a");
+    list3.add("B");
+    list3.add("c");
+    list3.add("D");
+    list3.add("e");
+    System.out.println("list3:");
+    System.out.println(list3.toString());
   } // end main
 } // end class
--- SuperLinkedList.java
+++ SuperLinkedList.java
@@ -1,31 +1,84 @@
 import java.util.LinkedList;
+import java.util.ListIterator;
 import java.util.Iterator;
 
 public class SuperLinkedList extends LinkedList<String> {
+    private String vowels = "aeiou";
+
     public boolean removeVowels() {
-        return false;
+        Iterator iter = this.iterator();
+        boolean found = false;
+        while (iter.hasNext()){
+            if(vowels.indexOf(iter.next().toString().toLowerCase()) != -1){
+                found = true;
+                iter.remove();
+            }
+        }
+        return found;
     }
 
     public boolean removeConsonants() {
-        return false;
+        Iterator iter = this.iterator();
+        boolean found = false;
+        while (iter.hasNext()){
+            if(vowels.indexOf(iter.next().toString().toLowerCase()) == -1){
+                found = true;
+                iter.remove();
+            }
+        }
+        return found;
     }
 
     public LinkedList<String> removeDuplicates() {
-        return null;
+        Iterator iter = this.iterator();
+        LinkedList list = new LinkedList();
+        for (int i = 0; i < size(); i++){
+            boolean skip = true;
+            iter = this.iterator();
+            while (iter.hasNext()){
+                if (get(i) == iter.next().toString()){
+                    if (!skip){
+                        list.add(get(i));
+                        iter.remove();
+                        i--;
+                    }
+                    skip = false;
+                }
+            }
+        }
+        return list;
     }
 
     public LinkedList<String> concatenateStrings() {
-
-        return null;
+        LinkedList list = new LinkedList();
+        ListIterator iter = this.listIterator();
+        iter.next();
+        while (iter.hasNext()){
+            String val = iter.previous().toString();
+            iter.next();
+            list.add(val + iter.next()toString());
+        }
+        return list;
     }
 
     public LinkedList<String> mix(LinkedList<String> list2) {
-
-        return null;
+        LinkedList list = new LinkedList();
+        Iterator iter = this.iterator();
+        Iterator iter2 = list2.iterator();
+        while (iter.hasNext()){
+            list.add(iter.next().toString());
+            list.add(iter2.next().toString());
+        }
+        return list;
     }
 
     public String toString() {
-
-        return "";
+        Iterator iter = this.iterator();
+        String result = "";
+        while (iter.hasNext()){
+            result += iter.next().toString() + ", ";
+        }
+        result = result.substring(0, result.length()-2);
+        return result;
     }
 } // end SuperLinkedList
